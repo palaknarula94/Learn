0ec09e0c3fd770b3d06093c2b58b80d3f9f94c13
palaknarula94 <palak.narula123@gmail.com>
2023-08-16 01:25:18 +0530

    2nd prototype

---
+++ Assets/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float moveSpeed;
+    public float xRange=10f;
+    float horizontalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * moveSpeed);
+
+        if (transform.position.x<-xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
